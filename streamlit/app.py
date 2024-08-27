0432e6be00bd62d68ba6ebf16f1b7eb7257b9287
Mehmet Arslan <103227997+Argetlam84@users.noreply.github.com>
2024-08-27 17:03:58 +0300

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -46,7 +46,6 @@ if page == "Main":
     st.title("Main Page")
     image_url ="https://github.com/user-attachments/assets/200257fb-04b2-4a16-940f-1743812afad3"
     st.image(image_url, use_column_width=True)
-    st.write("This is the main page of the Streamlit app.")
     st.write("""
     ### Time Series Forecasting App
     This application allows you to explore various time series forecasting models 
